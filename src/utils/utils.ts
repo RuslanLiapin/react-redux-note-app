046ba58d69bcf909e9d3944fde0c00e9bd1b31a4
Ruslan Liapin <joy.esenin@gmail.com>
2023-08-04 00:59:34 +0300

    utils added

---
+++ src/utils/utils.ts
@@ -0,0 +1,4 @@
+export const extractDatesFromContent = (content: string) => {
+  const dateRegex = /\b\d{1,2}\/\d{1,2}\/\d{4}\b|\b\d{4}-\d{2}-\d{2}\b|\b\d{2}\.\d{2}\.\d{4}\b/g;
+  return content.match(dateRegex) || [];
+};
